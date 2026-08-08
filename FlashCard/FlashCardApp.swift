0084068dc2021d0528955f69518a692e2d76bb37
gaylelemuel.bit <gaylelemuel@gmail.com>
2026-08-08 12:23:59 -0500

    Initial Commit

---
+++ FlashCard/FlashCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashCardApp.swift
+//  FlashCard
+//
+//  Created by Lemuel Gayle on 8/8/26.
+//
+
+import SwiftUI
+
+@main
+struct FlashCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
